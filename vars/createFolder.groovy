3dfd716f136fc709fc378cd09ab89c2f0721ed0a
elsebasan <elsebasan@gmail.com>
2022-02-09 17:43:30 -0300

    Changes to be committed: 	modified:   vars/createFolder.groovy 	new file:   vars/curl.groovy

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -1,7 +1,6 @@
 
-
-
-
+#!/usr/bin/env groovy
+/*
 def call(String urlString, String folderName, String userString, String tokenString) {
         //armo la url a para hacer el post 
         urlString = urlString + "createItem?name=" + folderName + "&mode=com.cloudbees.hudson.plugins.folder.Folder"
@@ -26,3 +25,19 @@ def call(String urlString, String folderName, String userString, String tokenStr
         salida = conn.getResponseCode()
         echo "${salida}"
 }
+*/
+def call(String urlString, String folderName, String userString, String tokenString) {
+//curl -XPOST "$SERVER/createItem?name=$FOLDERNAME&mode=com.cloudbees.hudson.plugins.folder.Folder" -H 'Content-Type: application/json' -d "$JSON" --user "$USER:$TOKEN"
+
+    cmd = "curl -XPOST '${urlString}'"
+    cmd << "/createItem?name="
+    cmd << '${folderName}"
+    cmd << "&mode=com.cloudbees.hudson.plugins.folder.Folder" 
+    cmd << " -H 'Content-Type: application/json' -d {}" 
+    TODO
+    cmd << "--user "$USER:$TOKEN"
+    cmd
+
+    statusCode = sh (returnStatus: true, script: "x${cmd.join(' ').trim()}")
+
+
