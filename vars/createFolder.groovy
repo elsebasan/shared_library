358ccb237d504a4a1e459501d37c289a1543b121
elsebasan <elsebasan@gmail.com>
2022-02-10 09:43:37 -0300

    pruebas parametros entrada

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -1,31 +1,5 @@
-
 #!/usr/bin/env groovy
-/*
-def call(String urlString, String folderName, String userString, String tokenString) {
-        //armo la url a para hacer el post 
-        urlString = urlString + "createItem?name=" + folderName + "&mode=com.cloudbees.hudson.plugins.folder.Folder"
-        def url = new URL(urlString)
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection()
-        //set credentials
-        String userpass = userString + ":" + tokenString
-        String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()))
-        //armo el metodo y los headers
-        conn.setRequestMethod("POST")
-        conn.setRequestProperty ("Authorization", basicAuth)
-        conn.setRequestProperty("Content-Type", "application/json")
-        conn.setDoOutput(true)
-        // Le paso un json vacio
-        OutputStream os = conn.getOutputStream()
-        OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8")
-        osw.write("{}")
-        osw.flush()
-        osw.close()
-        os.close()
-        conn.connect()
-        salida = conn.getResponseCode()
-        echo "${salida}"
-}
-*/
+
 def call(String urlString, String folderName, String userString, String tokenString) {
 //curl -XPOST "$SERVER/createItem?name=$FOLDERNAME&mode=com.cloudbees.hudson.plugins.folder.Folder" -H 'Content-Type: application/json' -d "$JSON" --user "$USER:$TOKEN"
     List cmd = []
