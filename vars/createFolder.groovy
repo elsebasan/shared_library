22c9037a8eb874e846a4b38d35a9cd0495e972c2
elsebasan <elsebasan@gmail.com>
2022-02-10 10:05:54 -0300

    pruebas parametros entrada

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -1,36 +1,6 @@
 #!/usr/bin/env groovy
 
-
-
 def call(String name = 'human') {
   echo "Hello, ${name}."
-       // folderCreate("http://localhost:8080/","pruebaFolder","seba", "tokenprueba")
 }
 
-
-/*
-def folderCreate(String urlString, String folderName, String userString, String tokenString) {
-//curl -XPOST "$SERVER/createItem?name=$FOLDERNAME&mode=com.cloudbees.hudson.plugins.folder.Folder" -H 'Content-Type: application/json' -d "$JSON" --user "$USER:$TOKEN"
-    List cmd = []
-    curlString = "curl -XPOST " + urlString + "/createItem?name=" + folderName + "&mode=com.cloudbees.hudson.plugins.folder.Folder"
-
-    cmd << curlString
-    cmd << "-H 'Content-Type: application/json'"
-    cmd << '-d "{}"'
-    userToken = "--user " + userString + ":" + tokenString
-    cmd << userToken
-
-    statusCode = sh (returnStatus: true, script: "${cmd.join(' ').trim()}")
-    println statusCode
-    cmd = "curl -XPOST '${urlString}'"
-    cmd << "/createItem?name="
-    cmd << '${folderName}"
-    cmd << "&mode=com.cloudbees.hudson.plugins.folder.Folder" 
-    cmd << " -H 'Content-Type: application/json' -d {}" 
-    TODO
-    cmd << "--user "$USER:$TOKEN"
-    cmd
-
-    statusCode = sh (returnStatus: true, script: "x${cmd.join(' ').trim()}")
-*/
-}
